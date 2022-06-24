35a3f68f91c95a1399509f489eedb5499d4eb495
Emily Daykin <emilydaykin31@gmail.com>
2022-06-24 18:21:41 +0200

    added google sign in to SignIn page

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,15 @@
+import { signInWithGooglePopup } from '../firebase/firebase.utils';
+
 const SignIn = () => {
+  const logGoogleUser = async () => {
+    const response = await signInWithGooglePopup();
+    console.log('response', response);
+  };
+
   return (
     <div>
       <h1>Sign In Page</h1>
+      <button onClick={logGoogleUser}>Sign in with Google</button>
     </div>
   );
 };
