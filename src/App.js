1031baec29d7e39ecc8d7d40b3ac9ab0ddfc82b7
Emily Daykin <emilydaykin31@gmail.com>
2022-06-24 14:43:17 +0200

    Added navbar to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
 import Home from './components/Home';
+import Navbar from './components/Navbar';
 
 const Shop = () => {
   return <h1>Shop Page</h1>;
@@ -10,8 +10,10 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<Home />} />
-        <Route path='/shop' element={<Shop />} />
+        <Route path='/' element={<Navbar />}>
+          <Route index element={<Home />} />
+          <Route path='/shop' element={<Shop />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
