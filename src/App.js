a164012fa392d993d034c47daf761db96a2d2748
Emily Daykin <emilydaykin31@gmail.com>
2022-06-24 13:37:49 +0200

    installed react router dom and moved categories to home component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import Categories from './components/Categories';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './components/Home';
+
+const Shop = () => {
+  return <h1>Shop Page</h1>;
+};
 
 const App = () => {
-  return <Categories />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path='/shop' element={<Shop />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
