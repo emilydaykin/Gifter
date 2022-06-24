7b65caa15db7412e4daca861c261bbc417b23959
Emily Daykin <emilydaykin31@gmail.com>
2022-06-24 13:38:51 +0200

    Added and styled navbar and logo

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Outlet, Link } from 'react-router-dom';
+import { ReactComponent as GiftLogo } from '../assets/ecommerce_gift.svg';
+
+const Navbar = () => {
+  return (
+    <>
+      <div className='navbar'>
+        <Link className='navbar__link navbar__link--main' to='/'>
+          <GiftLogo className='navbar__logo' />
+          <h1 className='navbar__heading'>Gifter</h1>
+        </Link>
+        <div className='navbar__links'>
+          <Link className='navbar__link' to='/shop'>
+            Shop
+          </Link>
+        </div>
+      </div>
+      <Outlet /> {/* Everything else will be rendered below the nav bar now! */}
+    </>
+  );
+};
+
+export default Navbar;
