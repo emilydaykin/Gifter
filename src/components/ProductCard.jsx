6d2de981ea855a33a9d14d4599a5c59a3e53cc8f
Emily Daykin <emilydaykin31@gmail.com>
2022-06-27 12:18:52 +0100

    added add-to-cart functionality in cart context and product card

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,8 +1,17 @@
+import { useContext } from 'react';
+import { CartContext } from '../contexts/cart.context';
+
 const ProductCard = ({ product }) => {
+  const { addItemToCart } = useContext(CartContext);
+
+  const addProductToCart = () => addItemToCart(product);
+
   return (
     <div className='product-card'>
       <img className='product-card__image' src={product.imageUrl} alt={product.name} />
-      <button className='button product-card__button'>Add to Card</button>
+      <button className='button product-card__button' onClick={addProductToCart}>
+        Add to Card
+      </button>
       <div className='product-card__product-info'>
         <h3 className='product-card__product-name'>{product.name}</h3>
         <p className='product-card__product-price'>&euro;{product.price}</p>
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -2,11 +2,28 @@ import { createContext, useState } from 'react';
 
 export const CartContext = createContext({
   isCartOpen: false,
-  setIsCartOpen: () => {}
+  setIsCartOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {}
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
-  const value = { isCartOpen, setIsCartOpen };
+  const [cartItems, setCartItems] = useState([]);
+
+  const addItemToCart = (productToAdd) => {
+    const matchingItemIndex = cartItems.findIndex((item) => item.id === productToAdd.id);
+
+    if (matchingItemIndex === -1) {
+      // item not yet in cart
+      setCartItems([...cartItems, { ...productToAdd, quantity: 1 }]);
+    } else {
+      // item already in cart
+      setCartItems([...cartItems, cartItems[matchingItemIndex].quantity++]);
+    }
+  };
+
+  const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems };
+
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
