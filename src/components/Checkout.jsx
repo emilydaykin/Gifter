454947181d49f7daccf8129402fd95b8264f34a8
Emily Daykin <emilydaykin31@gmail.com>
2022-07-01 19:39:09 +0200

    checkout styling done

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,43 +1,45 @@
 import { useContext } from 'react';
 import { CartContext } from '../contexts/cart.context';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretRight, faCaretLeft } from '@fortawesome/free-solid-svg-icons';
 
 const Checkout = () => {
-  const { cartItems } = useContext(CartContext);
+  const { cartItems, getCartTotalPrice } = useContext(CartContext);
+
   console.log('cartItems', cartItems);
-  const data = {
-    id: 1,
-    name: '2-Piece Mug Set',
-    imageUrl:
-      'https://images.unsplash.com/photo-1542556398-95fb5b9f9b48?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bXVnc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
-    price: 10,
-    quantity: 2
-  };
+
   return (
     <>
-      <h1>Checkout component</h1>
       <div className='checkout'>
-        <div className='checkout__heading'>
-          <p>Product</p>
-          <p>Description</p>
-          <p>Quanity</p>
-          <p>Price (&euro;)</p>
-          <p>Remove</p>
-        </div>
-        {cartItems.map((item) => (
-          <div key={item.id} className='checkout__item'>
-            <div className='checkout__item-image-container'>
-              <img className='checkout__item-image' src={item.imageUrl} alt={item.name} />
-            </div>
-            <span>{item.name}</span>
-            <div className='checkout__item-quantity'>
-              <span>&lt;</span>
-              <span>{item.quantity}</span>
-              <span>&gt;</span>
-            </div>
-            <span>{item.price}</span>
-            <span>X</span>
+        <div className='checkout__table'>
+          <div className='checkout__heading-row'>
+            <p className='checkout__heading checkout__heading--product'>Product</p>
+            <p className='checkout__heading checkout__heading--description'>Description</p>
+            <p className='checkout__heading checkout__heading--quantity'>Quanity</p>
+            <p className='checkout__heading checkout__heading--price'>Price (&euro;)</p>
+            <p className='checkout__heading checkout__heading--remove'>Remove</p>
           </div>
-        ))}
+          {cartItems.map((item) => (
+            <div key={item.id} className='checkout__item'>
+              <div className='checkout__item-image-container'>
+                <img className='checkout__item-image' src={item.imageUrl} alt={item.name} />
+              </div>
+              <div className='checkout__item-description'>{item.name}</div>
+              <div className='checkout__item-quantity'>
+                <FontAwesomeIcon className='checkout__item-quantity-icon' icon={faCaretLeft} />
+                &ensp;
+                <span>{item.quantity}</span>
+                &ensp;
+                <FontAwesomeIcon className='checkout__item-quantity-icon' icon={faCaretRight} />
+              </div>
+              <div className='checkout__item-price'>{item.price}</div>
+              <div className='checkout__item-remove'>&times;</div>
+            </div>
+          ))}
+          <div className='checkout__total'>
+            Total: &euro;<span>{getCartTotalPrice()}</span>
+          </div>
+        </div>
       </div>
     </>
   );
