8161c677ad168aec85a002642028f6d730373bc4
Emily Daykin <emilydaykin31@gmail.com>
2022-07-02 10:56:41 +0200

    styling fixes to cart dropdown items

--- src/components/cart/CartItem.jsx
+++ src/components/cart/CartItem.jsx
@@ -1,7 +1,9 @@
 const CartItem = ({ cartItem }) => {
   return (
     <div className='cart-item'>
-      <img className='cart-item__image' src={cartItem.imageUrl} alt={cartItem.name} />
+      <div className='cart-item__image'>
+        <img src={cartItem.imageUrl} alt={cartItem.name} />
+      </div>
       <div className='cart-item__text'>
         <h2 className='cart-item__name'>{cartItem.name}</h2>
         <span>
