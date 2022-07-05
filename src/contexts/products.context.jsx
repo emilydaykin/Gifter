60abfe5d0529c3ed7b98b3798c45085b02eaea3a
Emily Daykin <emilydaykin31@gmail.com>
2022-07-05 20:36:32 +0200

    added getCategoriesAndDocuments to products context useEffect to retrieve the data

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -2,7 +2,8 @@ import { createContext, useState, useEffect } from 'react';
 
 import allProducts from '../data/shop-data.js';
 
-import { addCollectionAndDocuments } from '../firebase/firebase.utils';
+// import { addCollectionAndDocuments } from '../firebase/firebase.utils';
+import { getCategoriesAndDocuments } from '../firebase/firebase.utils';
 
 export const ProductsContext = createContext({
   products: []
@@ -14,7 +15,12 @@ export const ProductsProvider = ({ children }) => {
 
   // useEffect cus we wanna fire this off just once to initialise Firestore
   useEffect(() => {
-    addCollectionAndDocuments('categories', allProducts); // 'categories' is the name i want for the collection
+    // addCollectionAndDocuments('categories', allProducts); // 'categories' is the name i want for the collection
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      console.log('categoryMap', categoryMap);
+    };
+    getCategoriesMap();
   }, []);
 
   const value = { products };
