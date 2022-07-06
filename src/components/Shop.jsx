e6636da02d93ca131e231f583411c9c6f0b94bcc
Emily Daykin <emilydaykin31@gmail.com>
2022-07-06 17:09:36 +0200

    updated Shop component to read data from firestore db, and updated naming to categories context

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,24 +1,32 @@
 import { useContext } from 'react';
-import { ProductsContext } from '../contexts/products.context';
+import { CategoriesContext } from '../contexts/categories.context';
 import ProductCard from './ProductCard';
 
 const Shop = () => {
-  const { products } = useContext(ProductsContext);
+  const { categoriesMap } = useContext(CategoriesContext);
+
+  const redirectToCategory = (category) => {
+    console.log(`${category} category clicked!`);
+  };
 
   return (
     <div className='shop'>
-      <h1 className='shop__category'>Birthday Gifts</h1>
-      <div className='product-group'>
-        {products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-      </div>
-      <h1 className='shop__category'>Christmas Gifts</h1>
-      <div className='product-group'>
-        {products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-      </div>
+      {Object.keys(categoriesMap).map((title) => (
+        <div key={title} className='shop__section'>
+          <h1 className='shop__category'>
+            {title} gifts <span onClick={() => redirectToCategory(title)}>&rarr;</span>
+          </h1>
+          <div className='product-group'>
+            {categoriesMap[title].map((product) => {
+              return product.id <= 4 ? (
+                <ProductCard key={product.id} product={product} />
+              ) : (
+                <p key={product.id}></p>
+              );
+            })}
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
