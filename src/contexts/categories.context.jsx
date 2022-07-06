e5b15101eace6d59929040d2ef8ebbe768506937
Emily Daykin <emilydaykin31@gmail.com>
2022-07-06 17:08:44 +0200

    renamed products context to categories context

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -5,13 +5,14 @@ import allProducts from '../data/shop-data.js';
 // import { addCollectionAndDocuments } from '../firebase/firebase.utils';
 import { getCategoriesAndDocuments } from '../firebase/firebase.utils';
 
-export const ProductsContext = createContext({
-  products: []
+export const CategoriesContext = createContext({
+  // products: []
+  categoriesMap: {}
 });
 
-export const ProductsProvider = ({ children }) => {
+export const CategoriesProvider = ({ children }) => {
   // const [products, setProducts] = useState(allProducts);
-  const [products, setProducts] = useState([]);
+  const [categoriesMap, setCategoriesMap] = useState({});
 
   // useEffect cus we wanna fire this off just once to initialise Firestore
   useEffect(() => {
@@ -19,10 +20,11 @@ export const ProductsProvider = ({ children }) => {
     const getCategoriesMap = async () => {
       const categoryMap = await getCategoriesAndDocuments();
       console.log('categoryMap', categoryMap);
+      setCategoriesMap(categoryMap);
     };
     getCategoriesMap();
   }, []);
 
-  const value = { products };
-  return <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>;
+  const value = { categoriesMap };
+  return <CategoriesContext.Provider value={value}>{children}</CategoriesContext.Provider>;
 };
