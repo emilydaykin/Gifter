8dd2b9959011b9dc0b62c386a30f77ef32169b0e
Emily Daykin <emilydaykin31@gmail.com>
2022-07-07 13:06:48 +0200

    updated/fixed styling for product card and product group

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,13 +1,13 @@
 import { useContext } from 'react';
 import { CartContext } from '../contexts/cart.context';
 
-const ProductCard = ({ product }) => {
+const ProductCard = ({ product, preview }) => {
   const { addItemToCart } = useContext(CartContext);
 
   const addProductToCart = () => addItemToCart(product);
 
   return (
-    <div className='product-card'>
+    <div className={!preview ? 'product-card' : 'product-card product-card--preview'}>
       <img className='product-card__image' src={product.imageUrl} alt={product.name} />
       <button className='button product-card__button' onClick={addProductToCart}>
         Add to Cart
