aa772b7c7a21193b87f0c876369d3de601ab091e
Emily Daykin <emilydaykin31@gmail.com>
2022-07-07 13:02:58 +0200

    removed hard-coded category files

--- src/App.js
+++ src/App.js
@@ -5,26 +5,16 @@ import About from './components/About';
 import Shop from './components/Shop';
 import SignIn from './components/auth/SignIn';
 import Checkout from './components/Checkout';
-import Birthday from './components/categories/Birthday';
-import Christmas from './components/categories/Christmas';
-import Anniversary from './components/categories/Anniversary';
-import ThankYou from './components/categories/ThankYou';
-import Wedding from './components/categories/Wedding';
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Navbar />}>
         <Route index element={<Home />} />
-        <Route path='shop' element={<Shop />} />
+        <Route path='shop/*' element={<Shop />} />
         <Route path='about' element={<About />} />
         <Route path='auth' element={<SignIn />} />
         <Route path='checkout' element={<Checkout />} />
-        <Route path='birthday' element={<Birthday />} />
-        <Route path='christmas' element={<Christmas />} />
-        <Route path='anniversary' element={<Anniversary />} />
-        <Route path='thank-you' element={<ThankYou />} />
-        <Route path='wedding' element={<Wedding />} />
       </Route>
     </Routes>
   );
