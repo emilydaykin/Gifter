3376ebd5943349c6a3173ac801ee454842effcbe
Emily Daykin <emilydaykin31@gmail.com>
2022-07-11 15:34:09 +0200

    redux restructuring - created user actions, types and dispatch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
+import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { onAuthStateChangeListener, createUserDocumentFromAuth } from './firebase/firebase.utils';
+import { setCurrentUser } from './store/user/user.action';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import About from './components/About';
@@ -7,6 +11,19 @@ import SignIn from './components/auth/SignIn';
 import Checkout from './components/checkout/Checkout';
 
 const App = () => {
+  const dispatch = useDispatch(); // this will never change, so no need to pass it as a dependency array into the useEffect (but can do)
+
+  useEffect(() => {
+    // this is a permanently open listener (so must unmount it to avoid memory leaks):
+    const unsubscribe = onAuthStateChangeListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user); // if already exists, we simply get back userDocRef
+      }
+      dispatch(setCurrentUser(user));
+    });
+    return unsubscribe;
+  }, []);
+
   return (
     <Routes>
       <Route path='/' element={<Navbar />}>
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import './styles/main.scss';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
-import { UserProvider } from './contexts/user.context';
+// import { UserProvider } from './contexts/user.context';
 import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
 import reportWebVitals from './reportWebVitals';
@@ -15,13 +15,13 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <UserProvider>
-          <CategoriesProvider>
-            <CartProvider>
-              <App />
-            </CartProvider>
-          </CategoriesProvider>
-        </UserProvider>
+        {/* <UserProvider> */}
+        <CategoriesProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </CategoriesProvider>
+        {/* </UserProvider> */}
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
