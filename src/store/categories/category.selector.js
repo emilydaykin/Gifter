7712be970fb158dfaa0fd5309046ebbc8fbe8d5f
Emily Daykin <emilydaykin31@gmail.com>
2022-07-12 16:37:34 +0200

    using reselect to memoise the category selector

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,7 +1,20 @@
-// Helper callback
-export const selectCategoriesMap = (state) =>
-  state.categories.categories.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {}); // initial instance: empty object
+import { createSelector } from 'reselect'; // memoises selectors for you!
+
+const selectCategoryReducer = (state) => state.categories;
+// memoised selector:
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => categoriesSlice.categories // will only rerun if the output from selectCategoryReducer is different
+);
+
+// Helper callback memoised
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) =>
+    categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {})
+  // initial instance: empty object
+);
