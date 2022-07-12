09a4b1487bf2316defb48f168ac7d54d192416f0
Emily Daykin <emilydaykin31@gmail.com>
2022-07-12 21:45:57 +0200

    switching back to the manual logger middleware

--- src/store/store.js
+++ src/store/store.js
@@ -1,24 +1,23 @@
 import { compose, createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; // this will be by default local storage in most web browsers
-import logger from 'redux-logger';
-
 import { rootReducer } from './root-reducer';
+// import logger from 'redux-logger';
 
 // currying:
-// const loggerMiddleware = (store) => (next) => (action) => {
-//   // accounting for actions without types on them (i.e. not passed from us, e.g. via redux thunk)
-//   if (!action.type) {
-//     return next(action);
-//   }
-//   console.log('type:', action.type);
-//   console.log('payload:', action.payload);
-//   console.log('currentState:', store.getState());
+const loggerMiddleware = (store) => (next) => (action) => {
+  // accounting for actions without types on them (i.e. not passed from us, e.g. via redux thunk)
+  if (!action.type) {
+    return next(action);
+  }
+  console.log('type:', action.type);
+  console.log('payload:', action.payload);
+  console.log('currentState:', store.getState());
 
-//   next(action);
+  next(action);
 
-//   console.log('next state: ', store.getState());
-// };
+  console.log('next state: ', store.getState());
+};
 
 const persistConfig = {
   key: 'root',
@@ -28,8 +27,8 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middlewares = [logger]; // enhance our store
-// const middlewares = [loggerMiddleware]; // enhance our store
+// const middlewares = [logger]; // enhance our store
+const middlewares = [loggerMiddleware]; // enhance our store
 
 const composedEnhancers = compose(applyMiddleware(...middlewares));
 
