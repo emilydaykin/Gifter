d09c36f675c9a1965abc7d092bb79c857498b283
Emily Daykin <emilydaykin31@gmail.com>
2022-07-12 16:36:45 +0200

    creating a manual (curried) logger middleware

--- src/store/store.js
+++ src/store/store.js
@@ -3,7 +3,23 @@ import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
 
-const middlewares = [logger]; // enhance our store
+//currying:
+const loggerMiddleware = (store) => (next) => (action) => {
+  // accounting for actions without types on them (i.e. not passed from us, e.g. via redux thunk)
+  if (!action.type) {
+    return next(action);
+  }
+  console.log('type:', action.type);
+  console.log('payload:', action.payload);
+  console.log('currentState:', store.getState());
+
+  next(action);
+
+  console.log('next state: ', store.getState());
+};
+
+// const middlewares = [logger]; // enhance our store
+const middlewares = [loggerMiddleware]; // enhance our store
 
 const composedEnhancers = compose(applyMiddleware(...middlewares));
 
