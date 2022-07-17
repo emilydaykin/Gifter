ba46e83aabcbd3c038d9fbaa181bc1084a25208d
Emily Daykin <emilydaykin31@gmail.com>
2022-07-18 00:05:35 +0200

    Added macro to typed-redux-saga import

--- src/store/categories/category.saga.ts
+++ src/store/categories/category.saga.ts
@@ -1,4 +1,4 @@
-import { takeLatest, all, call, put } from 'typed-redux-saga';
+import { takeLatest, all, call, put } from 'typed-redux-saga/macro';
 import { getCategoriesAndDocuments } from '../../firebase/firebase.utils';
 import { fetchCategoriesSuccess, fetchCategoriesFailure } from './category.action';
 import { CATEGORIES_ACTION_TYPES } from './category.types';
