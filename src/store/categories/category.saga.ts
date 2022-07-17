8fd806551c9c92879a3f8030a91514f1a04a45c2
Emily Daykin <emilydaykin31@gmail.com>
2022-07-17 23:36:09 +0200

    converted category saga to typescript

--- src/store/categories/category.saga.ts
+++ src/store/categories/category.saga.ts
@@ -1,4 +1,4 @@
-import { takeLatest, all, call, put } from 'redux-saga/effects';
+import { takeLatest, all, call, put } from 'typed-redux-saga';
 import { getCategoriesAndDocuments } from '../../firebase/firebase.utils';
 import { fetchCategoriesSuccess, fetchCategoriesFailure } from './category.action';
 import { CATEGORIES_ACTION_TYPES } from './category.types';
@@ -7,19 +7,19 @@ import { CATEGORIES_ACTION_TYPES } from './category.types';
 export function* fetchCategoriesAsync() {
   try {
     // use `call` to turn it into an effect
-    const categoryArray = yield call(getCategoriesAndDocuments, 'categories'); // callable method & its params
-    yield put(fetchCategoriesSuccess(categoryArray)); // put is the dispatch inside a generator
+    const categoryArray = yield* call(getCategoriesAndDocuments); // callable method & its params
+    yield* put(fetchCategoriesSuccess(categoryArray)); // put is the dispatch inside a generator
   } catch (error) {
     console.log(`ERROR: ${error}`);
-    yield put(fetchCategoriesFailure(error));
+    yield* put(fetchCategoriesFailure(error as Error));
   }
 }
 
 export function* onFetchCategories() {
   // if many actions received, take the latest one
-  yield takeLatest(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START, fetchCategoriesAsync);
+  yield* takeLatest(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START, fetchCategoriesAsync);
 }
 
 export function* categoriesSaga() {
-  yield all([call(onFetchCategories)]); // this will pause execution of the below until it finishes
+  yield* all([call(onFetchCategories)]); // this will pause execution of the below until it finishes
 }
