41dcdcea0e85f8ab2d1d4bd38ffcdb7f39dc7129
Emily Daykin <emilydaykin31@gmail.com>
2022-07-19 15:18:37 +0200

    fixed ts bug in cart action to return from add/remove/reduce cart items functions

--- src/store/cart/cart.action.ts
+++ src/store/cart/cart.action.ts
@@ -1,6 +1,6 @@
 import { CategoryItem } from '../categories/category.types';
 import { CART_ACTION_TYPES, CartItem } from './cart.types';
-import { createAction, withMatcher, Action, ActionWithPayload } from '../../reducer/reducer.utils';
+import { createAction, withMatcher, ActionWithPayload } from '../../reducer/reducer.utils';
 
 export type SetIsCartOpen = ActionWithPayload<CART_ACTION_TYPES.SET_IS_CART_OPEN, boolean>;
 
@@ -14,7 +14,7 @@ export const setCartItems = withMatcher(
   (cartItems: CartItem[]): SetCartItems => createAction(CART_ACTION_TYPES.SET_CART_ITEMS, cartItems)
 );
 
-export const addItemToCart = (cartItems: CartItem[], productToAdd: CategoryItem): void => {
+export const addItemToCart = (cartItems: CartItem[], productToAdd: CategoryItem) => {
   const matchingItemIndex = cartItems.findIndex((item) => item.id === productToAdd.id);
 
   if (matchingItemIndex === -1) {
@@ -22,29 +22,24 @@ export const addItemToCart = (cartItems: CartItem[], productToAdd: CategoryItem)
     const updatedCartItems = [...cartItems, { ...productToAdd, quantity: 1 }];
 
     // createAction(CART_ACTION_TYPES.SET_CART_ITEMS, updatedCartItems);
-    setCartItems(updatedCartItems);
+    return setCartItems(updatedCartItems);
   } else {
     // item already in cart
     const updatedCartItems = cartItems.map((item) => {
       return item.id === productToAdd.id ? { ...item, quantity: item.quantity + 1 } : item;
     });
 
-    // createAction(CART_ACTION_TYPES.SET_CART_ITEMS, updatedCartItems);
-    setCartItems(updatedCartItems);
+    return setCartItems(updatedCartItems);
   }
 };
 
-export const removeItemFromCart = (cartItems: CartItem[], productToRemove: CartItem): void => {
+export const removeItemFromCart = (cartItems: CartItem[], productToRemove: CartItem) => {
   const updatedCartItems = cartItems.filter((item) => item.id !== productToRemove.id);
 
-  // createAction(CART_ACTION_TYPES.SET_CART_ITEMS, updatedCartItems);
-  setCartItems(updatedCartItems);
+  return setCartItems(updatedCartItems);
 };
 
-export const reduceItemQuantityInCart = (
-  cartItems: CartItem[],
-  productToReduce: CartItem
-): void => {
+export const reduceItemQuantityInCart = (cartItems: CartItem[], productToReduce: CartItem) => {
   const quantityOfItem = productToReduce.quantity;
 
   const reduceQuantity = cartItems.map((item) => {
@@ -55,6 +50,5 @@ export const reduceItemQuantityInCart = (
 
   const updatedCartItems = quantityOfItem > 1 ? reduceQuantity : removeItem;
 
-  // createAction(CART_ACTION_TYPES.SET_CART_ITEMS, updatedCartItems);
-  setCartItems(updatedCartItems);
+  return setCartItems(updatedCartItems);
 };
