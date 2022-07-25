edf2a30af976d26898de4dd8b62f4a500c48c88b
Emily Daykin <emilydaykin31@gmail.com>
2022-07-25 20:34:51 +0100

    fixed test that was failing with new React Suspense code

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,25 +1,38 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import App from '../App';
 import { Provider } from 'react-redux';
 import { store } from '../store/store';
 
-test('Gift categories are rendered', () => {
+test('Gift categories are rendered', async () => {
+  // await act(async () =>
   render(
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
+    // )
   );
 
-  const xmasCategory = screen.getByText(/christmas/i);
-  const bdayCategory = screen.getByText(/birthday/i);
-  const annivCategory = screen.getByText(/anniversary/i);
-  const tyCategory = screen.getByText(/thank you/i);
+  // waitFor better than wrapping the render with act()
+  await waitFor(() => {
+    const xmasCategory = screen.getByText(/christmas/i);
+    expect(xmasCategory).toBeInTheDocument();
+  });
 
-  expect(xmasCategory).toBeInTheDocument();
-  expect(bdayCategory).toBeInTheDocument();
-  expect(annivCategory).toBeInTheDocument();
-  expect(tyCategory).toBeInTheDocument();
+  await waitFor(() => {
+    const bdayCategory = screen.getByText(/birthday/i);
+    expect(bdayCategory).toBeInTheDocument();
+  });
+
+  await waitFor(() => {
+    const annivCategory = screen.getByText(/anniversary/i);
+    expect(annivCategory).toBeInTheDocument();
+  });
+
+  await waitFor(() => {
+    const tyCategory = screen.getByText(/thank you/i);
+    expect(tyCategory).toBeInTheDocument();
+  });
 });
