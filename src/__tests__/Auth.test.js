aec81e6489f31afd48313877042f2e0a06bade79
Emily Daykin <emilydaykin31@gmail.com>
2022-07-27 14:34:54 +0100

    new tests to Auth (for login inputs)

--- src/__tests__/Auth.test.js
+++ src/__tests__/Auth.test.js
@@ -1,10 +1,17 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { BrowserRouter } from 'react-router-dom';
 import SignIn from '../components/auth/SignIn';
+import LogIn from '../components/auth/LogIn';
 import { Provider } from 'react-redux';
 import { store } from '../store/store';
 
-test('Sign In page (/auth) section rendered correctly', () => {
+const mockUser = {
+  email: 'mock@user.com',
+  password: 'mockPassword1!'
+};
+
+test('Sign In page (/auth) buttons rendered correctly', () => {
   render(
     <Provider store={store}>
       <BrowserRouter>
@@ -13,7 +20,9 @@ test('Sign In page (/auth) section rendered correctly', () => {
     </Provider>
   );
 
-  const buttons = screen.getAllByRole('button');
+  const buttons = screen.getAllByRole('button', { className: /button/i });
+
+  // const buttons = screen.getAllByRole('button');
   expect(buttons.length).toEqual(3);
 
   const expectedButtons = ['log in!', 'register!', 'log in with google'];
@@ -22,4 +31,55 @@ test('Sign In page (/auth) section rendered correctly', () => {
   expectedButtons.forEach((button) => {
     expect(actualButtons).toContainEqual(button);
   });
+});
+
+test('Sign In page (/auth) headings displayed correctly', () => {
+  render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <SignIn />
+      </BrowserRouter>
+    </Provider>
+  );
+
+  const logInHeader = screen.getByText(/i already have an account/i);
+  expect(logInHeader).toBeInTheDocument();
+
+  const registerHeader = screen.getByText(/i don't have an account/i);
+  expect(registerHeader).toBeInTheDocument();
+
+  const subHeaders = screen.getAllByText(/with your email and password/i);
+  expect(subHeaders.length).toEqual(2);
+});
+
+test('Assert log in user inputs are accepted and displayed correctly', () => {
+  render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <LogIn />
+      </BrowserRouter>
+    </Provider>
+  );
+
+  const emailInput = screen.getByLabelText('Email', { selector: 'input' });
+  const passwordInput = screen.getByLabelText('Password', { selector: 'input' });
+
+  // console.log('logInEmailInput', logInEmailInput);
+  // console.log('logInEmailInput.length', logInEmailInput.length);
+
+  // Get the form's button element with a (case-insensitive) classname of 'button'
+  // const submitButton = screen.getByRole('button', { className: /button/i });
+
+  expect(emailInput).toBeInTheDocument();
+  expect(passwordInput).toBeInTheDocument();
+
+  // simulate user typing in email & pw to form's input fields:
+  userEvent.type(emailInput, mockUser.email);
+  userEvent.type(passwordInput, mockUser.password);
+
+  // emailInput.value is from emailInput.pendingProps.value
+  // console.log('emailInput', emailInput);
+  // console.log('emailInput.value', emailInput.value);
+  expect(emailInput.value).toEqual(mockUser.email);
+  expect(passwordInput.value).toEqual(mockUser.password);
 });
